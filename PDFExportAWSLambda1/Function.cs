dadea798b333208a2251308b60aea93f8433d1b3
Kuni <kuni.ujiie@grapecity.com>
2024-03-07 14:06:00 +0900

    .NETとDioDocsのライブラリを更新
    .NET6、DioDocs V7Jに更新

--- PDFExportAWSLambda1/Function.cs
+++ PDFExportAWSLambda1/Function.cs
@@ -1,63 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using GrapeCity.Documents.Pdf;
 using GrapeCity.Documents.Text;
+using System.Drawing;
+using System.Net;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace PDFExportAWSLambda1
+namespace PDFExportAWSLambda1;
+
+public class Function
 {
-    public class Function
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
+        APIGatewayProxyResponse response;
 
-        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+        string? queryString;
+        input.QueryStringParameters.TryGetValue("name", out queryString);
+
+        string Message = string.IsNullOrEmpty(queryString)
+            ? "Hello, World!!"
+            : $"Hello, {queryString}!!";
+
+        //GcPdfDocument.SetLicenseKey("製品版またはトライアル版のライセンスキーを設定");
+
+        GcPdfDocument doc = new GcPdfDocument();
+        GcPdfGraphics g = doc.NewPage().Graphics;
+
+        g.DrawString(Message,
+            new TextFormat() { Font = StandardFonts.Helvetica, FontSize = 12 },
+            new PointF(72, 72));
+
+        var base64String = "";
+
+        using (var ms = new MemoryStream())
         {
-            APIGatewayProxyResponse response;
-
-            string queryString;
-            input.QueryStringParameters.TryGetValue("name", out queryString);
-
-            string Message = string.IsNullOrEmpty(queryString)
-                ? "Hello, World!!"
-                : $"Hello, {queryString}!!";
-
-            //GcPdfDocument.SetLicenseKey("");
-
-            GcPdfDocument doc = new GcPdfDocument();
-            GcPdfGraphics g = doc.NewPage().Graphics;
-
-            g.DrawString(Message,
-                new TextFormat() { Font = StandardFonts.Helvetica, FontSize = 12 },
-                new PointF(72, 72));
-
-            var base64String = "";
-
-            using (var ms = new MemoryStream())
-            {
-                doc.Save(ms, false);
-                base64String = Convert.ToBase64String(ms.ToArray());
-            }
-
-            response = new APIGatewayProxyResponse
-            {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = base64String,
-                IsBase64Encoded = true,
-                Headers = new Dictionary<string, string> {
-                    { "Content-Type", "application/pdf" },
-                    { "Content-Disposition", "attachment; filename=Result.pdf"},
-                }
-            };
-
-            return response;
+            doc.Save(ms, false);
+            base64String = Convert.ToBase64String(ms.ToArray());
         }
+
+        response = new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = base64String,
+            IsBase64Encoded = true,
+            Headers = new Dictionary<string, string> {
+            {"Content-Type", "application/pdf"},
+            {"Content-Disposition", "attachment; filename=Result.pdf"},
+        }
+        };
+
+        return response;
     }
 }
